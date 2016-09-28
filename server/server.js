8e945aa8f260189f3fb0a0ef9044a5da9e3d23f2
Thomas <jtruant@gmx.com>
2016-09-28 00:14:58 -0600

    socketify the server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { Server } from 'http';
 
+
 const app = express();
 const http = Server(app);
+const  io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'client' });
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 http.listen(3000, () => {
