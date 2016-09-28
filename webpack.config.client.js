0ddf1db63ecdeac7237e4721e359c780cc8785b0
Thomas <jtruant@gmx.com>
2016-09-27 23:27:11 -0600

    es6

---
+++ webpack.config.client.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  entry: './client/js/client.js',
+  output: {
+    path: './client/js/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
---
+++ webpack.config.server.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './server/server.js',
+  target: 'node',
+  output: {
+    path: './server/',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  externals: nodeModules
+};
