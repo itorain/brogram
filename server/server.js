5b75bfefc229545c74f735e49159413201d2d480
Isaiah <itorain12@gmail.com>
2016-09-28 01:12:28 -0600

    Fixed eslint

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
+/* eslint new-cap: ["error", { "capIsNewExceptions": ["Server"] }] */
 import express from 'express';
 import { Server } from 'http';
 
-
 const app = express();
 const http = Server(app);
-const  io = require('socket.io')(http);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'client' });
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on('connection', socket => {
+    console.log('a user connected');
 });
 
 http.listen(3000, () => {
